987dfafacd1a4f03d8ce8192f5adefd7d4532ad8
allu <aleksijaakkojuhani@gmail.com>
2020-11-24 11:56:24 +0200

    update requireAuth middleware

--- middlewares/requireAuth2.js
+++ middlewares/requireAuth2.js
@@ -2,23 +2,28 @@ const jwt_decode = require('jwt-decode');
 const User = require('../models/User');
 
 module.exports = async function (req, res, next) {
-  const { cookies } = req;
-  const session = cookies['next-auth.session-token'];
-
-  if (!session) {
-    console.log('no session token');
+  const { authorization } = req.headers;
+  console.log('auth header ', authorization);
+  if (!authorization || !authorization.includes('Bearer')) {
     return res.status(401).send('unauthorized');
   }
 
-  const decoded = jwt_decode(session);
-  const user = await User.findOne({
-    usernameLowerCase: decoded.name.toLowerCase(),
-  });
+  try {
+    const token = authorization.split(' ')[1];
+    const decoded = jwt_decode(token);
+    console.log('jeejee? ', decoded);
+    const user = await User.findOne({
+      usernameLowerCase: decoded.name.toLowerCase(),
+    });
 
-  if (!user) {
-    return res.status(400).send('user not found');
+    if (!user) {
+      return res.status(400).send('user not found');
+    }
+
+    req.currentUser = user;
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send('server error...');
   }
-
-  req.currentUser = user;
-  return next();
 };
